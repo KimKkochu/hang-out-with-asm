9815023563fc65a2e02549b5fb6c07639f909228
KimKkochu <kimkkochu69@gmail.com>
2022-12-29 17:14:57 +0900

    input for echo(1 step)

---
+++ echo.asm
@@ -0,0 +1,15 @@
+global _start
+
+section .text
+_start:
+    sub rsp, 100 ; buff area allocate
+
+    mov rax, 0
+    mov rdi, 0 ; sys_read for rdi, 0 is standard input
+    mov rsi, rsp
+    mov rdx, 99 ; input string len
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
